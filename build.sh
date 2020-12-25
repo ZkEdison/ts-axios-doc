5d0a6a152d06dae838256b9ae2253d3b110436cd
zhangkang <zhangk@shanhs.com>
2020-12-25 12:00:20 +0800

    'config'

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -env
+
+rm -rf dist
+
+npm run build
+
+cd dist
+
+git init 
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:ZkEdison/ts-axios-doc.git master:gh-pages
